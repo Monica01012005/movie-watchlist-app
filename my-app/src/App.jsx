817658f9c28fb084e6aaaf3a5ff164d12268b56e
chandan <chandanjingade02@gmail.com>
2026-04-13 19:34:48 +0530

    frontend setup

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-purple-600 flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">
+        IT WORKS 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
